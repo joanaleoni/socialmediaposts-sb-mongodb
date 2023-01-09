00c452972e28f85efa2a989e1b945505845297f7
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 13:04:44 -0300

    Endpoint to return post by id

---
+++ src/main/java/com/joana/socialmediaposts/services/PostService.java
@@ -0,0 +1,22 @@
+package com.joana.socialmediaposts.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joana.socialmediaposts.domain.Post;
+import com.joana.socialmediaposts.repositories.PostRepository;
+import com.joana.socialmediaposts.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+	}
+}
