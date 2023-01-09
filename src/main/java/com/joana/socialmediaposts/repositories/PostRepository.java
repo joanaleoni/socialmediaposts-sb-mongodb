e98ced65c486db4bf790e8116c032ea8c950e244
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 13:38:46 -0300

    Find posts by title search using query methods

--- src/main/java/com/joana/socialmediaposts/repositories/PostRepository.java
+++ src/main/java/com/joana/socialmediaposts/repositories/PostRepository.java
@@ -1,9 +1,13 @@
 package com.joana.socialmediaposts.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.joana.socialmediaposts.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
--- src/main/java/com/joana/socialmediaposts/resources/PostResource.java
+++ src/main/java/com/joana/socialmediaposts/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.joana.socialmediaposts.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joana.socialmediaposts.domain.Post;
+import com.joana.socialmediaposts.resources.util.URL;
 import com.joana.socialmediaposts.services.PostService;
 
 @RestController
@@ -21,5 +25,12 @@ public class PostResource {
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping(value="/titlesearch")
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
 	}
 }
