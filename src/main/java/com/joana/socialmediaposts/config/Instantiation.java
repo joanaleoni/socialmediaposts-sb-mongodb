eacb7363576ea044890f30c2a4a64e140d0db641
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 13:15:44 -0300

    Adding comments to posts

--- src/main/java/com/joana/socialmediaposts/config/Instantiation.java
+++ src/main/java/com/joana/socialmediaposts/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.joana.socialmediaposts.domain.Post;
 import com.joana.socialmediaposts.domain.User;
 import com.joana.socialmediaposts.dto.AuthorDTO;
+import com.joana.socialmediaposts.dto.CommentDTO;
 import com.joana.socialmediaposts.repositories.PostRepository;
 import com.joana.socialmediaposts.repositories.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(user1));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user1));
+				
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(user2));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(user3));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(user2));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/joana/socialmediaposts/domain/Post.java
+++ src/main/java/com/joana/socialmediaposts/domain/Post.java
@@ -1,11 +1,14 @@
 package com.joana.socialmediaposts.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.joana.socialmediaposts.dto.AuthorDTO;
+import com.joana.socialmediaposts.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable {
@@ -16,6 +19,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 
@@ -65,6 +70,10 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}	
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
