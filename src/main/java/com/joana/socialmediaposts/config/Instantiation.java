200866283ac6213279e4b99bd8ccc61a47f67e34
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 11:01:25 -0300

    Database instantiation

---
+++ src/main/java/com/joana/socialmediaposts/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.joana.socialmediaposts.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.joana.socialmediaposts.domain.User;
+import com.joana.socialmediaposts.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User user1 = new User(null, "Maria Brown", "maria@gmail.com");
+		User user2 = new User(null, "Alex Green", "alex@gmail.com");
+		User user3 = new User(null, "Bob Grey", "bob@gmail.com");		
+		
+		userRepository.saveAll(Arrays.asList(user1, user2, user3));
+	}
+}
