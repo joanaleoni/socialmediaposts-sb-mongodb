058a4a0fc67edbc028985e0453b5f43a43dbae1c
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 11:14:43 -0300

    Removing unused import

--- src/main/java/com/joana/socialmediaposts/resources/UserResource.java
+++ src/main/java/com/joana/socialmediaposts/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.joana.socialmediaposts.resources;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
