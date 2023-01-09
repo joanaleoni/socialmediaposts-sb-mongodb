ab46ca0813ee7998244cbe8c106cc8abb7efc231
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 09:10:32 -0300

    Project created

---
+++ src/main/java/com/joana/socialmediaposts/SocialmediapostsApplication.java
@@ -0,0 +1,13 @@
+package com.joana.socialmediaposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediapostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediapostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joana/socialmediaposts/SocialmediapostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joana.socialmediaposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediapostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
