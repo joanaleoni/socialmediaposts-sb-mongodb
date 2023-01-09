b6cf9e091021ccc76a4631a16b2728290ae49979
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 12:52:26 -0300

    Associating posts to User

--- src/main/java/com/joana/socialmediaposts/config/Instantiation.java
+++ src/main/java/com/joana/socialmediaposts/config/Instantiation.java
@@ -41,5 +41,8 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user1));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		user1.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(user1);		
 	}
 }
--- src/main/java/com/joana/socialmediaposts/domain/User.java
+++ src/main/java/com/joana/socialmediaposts/domain/User.java
@@ -1,19 +1,25 @@
 package com.joana.socialmediaposts.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
+	@DBRef(lazy=true)
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {}
 
 	public User(String id, String name, String email) {
@@ -44,6 +50,10 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
 	}
 
 	@Override
